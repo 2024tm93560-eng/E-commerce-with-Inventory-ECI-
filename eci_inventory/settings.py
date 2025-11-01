bb96a497a5370a460e44b8f1438e43c536356f2e
Chithra <2024tm93560@wilp.bits-pilani.ac.in>
2025-11-01 15:44:40 +0530

    Resolve merge conflicts

--- eci_inventory/settings.py
+++ eci_inventory/settings.py
@@ -40,7 +40,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'inventory',
+    'drf_yasg',
 ]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -120,7 +122,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- eci_inventory/urls.py
+++ eci_inventory/urls.py
@@ -18,15 +18,36 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Inventory API",
+      default_version='v1',
+      description="API documentation for Inventory Service",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 def home(request):
     return HttpResponse("Welcome to ECI Inventory Service!")
 
+#urlpatterns = [
+    #path('', home),  # Optional homepage
+    #path('admin/', admin.site.urls),
+    #path('v1/', include('inventory.urls')), 
+   # from django.urls import path, include
+
 urlpatterns = [
-    path('', home),  # Optional homepage
-    path('admin/', admin.site.urls),
-    path('v1/', include('inventory.urls')), 
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('v1/', include('inventory.urls')),
 ]
+
 
 
 
